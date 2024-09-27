96fb0a538a7a4c473c2f913455663b4c59997419
Manuel Barone <baronemanuel05@gmail.com>
2024-09-27 16:56:19 +0200

    exception handling

--- UNIT-4-Java-Programming-1/src/main/Main.java
+++ UNIT-4-Java-Programming-1/src/main/Main.java
@@ -7,7 +7,7 @@ import aggr.Player;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
 		Player p = new Player();
 
@@ -18,52 +18,56 @@ public class Main {
 
 		boolean flag = false;
 		System.out.println("Dovrai aggiungere 5 elementi all'interno del player prima di poterlo usare!!!\n".toUpperCase());
-		
+
 		do {
-			
 			System.out.println(questions);
 			p.aggiungiElemento(in, Integer.parseInt(in.nextLine()), flag);
-			
+
 		}while(p.getElementi().size() < 5);
 
 		flag = false;
 
 		do {
-			
-			System.out.println("Quale elemento vuoi usare ? - [premi 0 se vuoi uscire dal programma]");
-			p.printElements();
-			switch(Integer.parseInt(in.nextLine())) {
-			case 1:
-				p.usaElementi(in, flag, 0);
-				break;
-				
-			case 2: 
-				p.usaElementi(in, flag, 1);
-				break;
-				
-			case 3:
-				p.usaElementi(in, flag, 2);
-				break;
-				
-			case 4:
-				p.usaElementi(in, flag, 3);
-				break;
-				
-			case 5:
-				p.usaElementi(in, flag, 4);
-				break;
-				
-			case 0:
-				flag = true;
-				System.out.println("Arrivederci!");
-				break;
-				
-			default:
-				System.out.println("Opzione non consentita!");
+
+			try {
+				System.out.println("Quale elemento vuoi usare ? - [premi 0 se vuoi uscire dal programma]");
+				p.printElements();
+				switch(Integer.parseInt(in.nextLine())) {
+				case 1:
+					p.usaElementi(in, flag, 0);
+					break;
+
+				case 2: 
+					p.usaElementi(in, flag, 1);
+					break;
+
+				case 3:
+					p.usaElementi(in, flag, 2);
+					break;
+
+				case 4:
+					p.usaElementi(in, flag, 3);
+					break;
+
+				case 5:
+					p.usaElementi(in, flag, 4);
+					break;
+
+				case 0:
+					flag = true;
+					System.out.println("Arrivederci!");
+					break;
+
+				default:
+					System.out.println("Opzione non consentita!");
+				}
+
+			}catch(Exception e) {
+				System.out.println("errore: " + e.getLocalizedMessage());
 			}
-			
+
 		}while(!flag);
-		
+
 	}
 
 }
