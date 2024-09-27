71a5691e378e98eeffe9c8796709177b4d25ef52
Manuel Barone <baronemanuel05@gmail.com>
2024-09-27 16:15:00 +0200

    refactor

--- UNIT-4-Java-Programming-1/src/main/Main.java
+++ UNIT-4-Java-Programming-1/src/main/Main.java
@@ -20,38 +20,48 @@ public class Main {
 		System.out.println("Dovrai aggiungere 5 elementi all'interno del player prima di poterlo usare!!!\n".toUpperCase());
 		
 		do {
+			
 			System.out.println(questions);
 			p.aggiungiElemento(in, Integer.parseInt(in.nextLine()), flag);
+			
 		}while(p.getElementi().size() < 5);
 
 		flag = false;
 
 		do {
+			
 			System.out.println("Quale elemento vuoi usare ? - [premi 0 se vuoi uscire dal programma]");
 			p.printElements();
 			switch(Integer.parseInt(in.nextLine())) {
 			case 1:
 				p.usaElementi(in, flag, 0);
 				break;
+				
 			case 2: 
 				p.usaElementi(in, flag, 1);
 				break;
+				
 			case 3:
 				p.usaElementi(in, flag, 2);
 				break;
+				
 			case 4:
 				p.usaElementi(in, flag, 3);
 				break;
+				
 			case 5:
 				p.usaElementi(in, flag, 4);
 				break;
+				
 			case 0:
 				flag = true;
 				System.out.println("Arrivederci!");
 				break;
+				
 			default:
 				System.out.println("Opzione non consentita!");
 			}
+			
 		}while(!flag);
 		
 	}
